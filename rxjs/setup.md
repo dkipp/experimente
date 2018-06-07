6ed3be84d364b3701f60d815287fecbd48f4eeb5
dkipp <mail@daniel-kipp.de>
2018-06-07 15:38:35 +0200

    first commit

---
+++ rxjs/setup.md
@@ -0,0 +1,5 @@
+yarn init -yp
+yarn add rxjs webpack webpack-dev-server typescript ts-loader
+yarn add webpack-cli --dev
+
+npx tsc --init
---
+++ rxjs/setup/src/code.ts
@@ -0,0 +1,25 @@
+import { Observable, timer, combineLatest } from 'rxjs';
+
+//https://coursetro.com/posts/code/148/RxJS-Observables-Tutorial---Creating-&-Subscribing-to-Observables
+//timerOne emits first value at 1s, then once every 4s
+const timerOne = timer(1000, 4000);
+//timerTwo emits first value at 2s, then once every 4s
+const timerTwo = timer(2000, 4000);
+//timerThree emits first value at 3s, then once every 4s
+const timerThree = timer(3000, 4000);
+
+//combineLatest also takes an optional projection function
+const combinedProject = combineLatest(
+  timerOne,
+  timerTwo,
+  timerThree,
+  (one, two, three) => {
+    return `Timer One (Proj) Latest: ${one}, 
+              Timer Two (Proj) Latest: ${two}, 
+              Timer Three (Proj) Latest: ${three}`;
+  }
+);
+//log values
+const subscribe = combinedProject.subscribe(latestValuesProject =>
+  console.log(latestValuesProject)
+);
